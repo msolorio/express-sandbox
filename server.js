e9080ba4c0097bd6787f2ebabf4b1cecaa5147ba
msolorio <msolorio789@gmail.com>
2017-05-20 11:31:00 -0700

    add functionality to generate madlib based on query params sent in

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const express = require('express');
 
 const app = express();
 
-const sentence = 'this is the sentence';
+function generateMadLib(wordInputs) {
+  return `There's a ${wordInputs.adjective1} new ${wordInputs.name} in ${wordInputs.place} and everyone's talking.
+Stunningly ${wordInputs.adjective2} and ${wordInputs.adverb} ${wordInputs.adjective3}, all the cool kids know it.
+However, ${wordInputs.name} has a secret - ${wordInputs.name}'s a vile vampire.
+
+Will it end with a bite, or with a stake through the ${wordInputs.noun}?`
+}
 
 app.get('/', (request, response) => {
+  const sentence = generateMadLib(request.query);
   response.send(sentence);
 });
 
